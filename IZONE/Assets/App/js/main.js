f1199dccd2693ccca574d3a1be4c05218602daee
Minh Ki Dong <minhkidong@gmail.com>
2019-12-05 12:47:48 +0700

    11
    11

--- IZONE/Assets/App/js/main.js
+++ IZONE/Assets/App/js/main.js
@@ -81,33 +81,37 @@ jQuery(document).ready(function() {
 
 
     function makeTimer() {
+        jQuery('.izone-timer').each(function(){
+            var endTimeValue = jQuery(this).attr('data-endtime');
+            var endTime = new Date(endTimeValue);
+            endTime = (Date.parse(endTime) / 1000);
 
-        //      var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");
-        var endTimeValue = jQuery("#izone-timer").attr('data-endtime');
-        var endTime = new Date(endTimeValue);
-        endTime = (Date.parse(endTime) / 1000);
+            var now = new Date();
+            now = (Date.parse(now) / 1000);
 
-        var now = new Date();
-        now = (Date.parse(now) / 1000);
+            var timeLeft = endTime - now;
 
-        var timeLeft = endTime - now;
+            var days = Math.floor(timeLeft / 86400);
+            var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+            var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+            var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
 
-        var days = Math.floor(timeLeft / 86400);
-        var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
-        var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+            if (hours < "10") { hours = "0" + hours; }
+            if (minutes < "10") { minutes = "0" + minutes; }
+            if (seconds < "10") { seconds = "0" + seconds; }
 
-        if (hours < "10") { hours = "0" + hours; }
-        if (minutes < "10") { minutes = "0" + minutes; }
-        if (seconds < "10") { seconds = "0" + seconds; }
-
-        $("#izone-days").html(days + "<span class='text-muted'>Ngày</span>");
-        $("#izone-hours").html(hours + "<span class='text-muted'>Giờ</span>");
-        $("#izone-minutes").html(minutes + "<span class='text-muted'>Phút</span>");
-        $("#izone-seconds").html(seconds + "<span class='text-muted'>Giây</span>");
+            jQuery(this).find(".izone-days").html(days + "<span class='text-muted'>Ngày</span>");
+            jQuery(this).find(".izone-hours").html(hours + "<span class='text-muted'>Giờ</span>");
+            jQuery(this).find(".izone-minutes").html(minutes + "<span class='text-muted'>Phút</span>");
+            jQuery(this).find(".izone-seconds").html(seconds + "<span class='text-muted'>Giây</span>");
+        });
 
     }
+    jQuery('.nano-content .gw-nav-list > li > a').click(function(e){
+        e.preventDefault();
+        jQuery(this).next('.gw-submenu').slideToggle();
 
+    });
     setInterval(function() { makeTimer(); }, 1000);
 
     //body_padding_top();
